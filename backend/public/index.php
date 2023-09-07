eae3f2ba4958a788e5b0883fd3cec25114bb1355
Moritz Herbstmeier <moritz.herbstmeier@neumannmueller.com>
2023-09-07 10:46:15 +0200

    added di container package

--- backend/public/index.php
+++ backend/public/index.php
@@ -6,13 +6,10 @@ use App\Controller\UserController;
 require __DIR__ . '/../vendor/autoload.php';
 
 $container = new Container();
-$container->set(UserController::class, function ($container) {
-    return new UserController();
-});
+AppFactory::setContainer($container); // Set Slim's default container to use yours
+$container->set('UserController', \DI\create(UserController::class));
 
 $app = AppFactory::create();
-$app->setContainer($container);
-
 require_once '../src/router.php';
 
 $app->run();
