02f5259ceb8d91b158a70c28f5a0419965017123
herbstmeier <49902849+herbstmeier@users.noreply.github.com>
2023-09-07 14:46:56 +0000

    Update deploy to output generated at 6f72f9f

--- assets/UserView-d8c3580f.js
+++
@@ -1,1 +0,0 @@
-import{d as e,_ as n,o,b as s}from"./index-b7a6dce0.js";const r=e({name:"UserView"});function t(a,c,p,_,i,d){return o(),s("div",null," User ")}const l=n(r,[["render",t]]);export{l as default};
