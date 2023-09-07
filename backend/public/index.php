466d8afb2170956f050657beb20ec3c307d8d964
Moritz Herbstmeier <moritz.herbstmeier@neumannmueller.com>
2023-09-07 11:04:37 +0200

    following the tutorial

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,7 +7,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $container = new Container();
 AppFactory::setContainer($container); // Set Slim's default container to use yours
-$container->set('UserController', \DI\create(UserController::class));
+$container->set('UserController', function() {
+    return new UserController();
+});
 
 $app = AppFactory::create();
 require_once '../src/router.php';
