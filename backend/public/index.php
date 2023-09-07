27091f22126f9867e3bcd0ff8a24d48c761af70c
herbstmeier <49902849+herbstmeier@users.noreply.github.com>
2023-09-07 10:05:35 +0000

    Update deploy to output generated at b0a2bd8

---
+++ backend/public/index.php
@@ -0,0 +1,19 @@
+<?php
+include '../config/config.php';
+use Slim\Factory\AppFactory;
+use DI\Container;
+require '../src/UserController.php';
+require __DIR__ . '/../vendor/autoload.php';
+require_once 'Database.php';
+
+$container = new Container();
+AppFactory::setContainer($container); // Set Slim's default container to use yours
+$container->set('UserController', function() {
+    return new \App\Controller\UserController(Database::getInstance());
+});
+
+$app = AppFactory::create();
+require_once '../src/router.php';
+
+$app->run();
+?>
